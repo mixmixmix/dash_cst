81f065647b17e051df2a9e94ec4f5f4d469c762a
mixmixmix <mikolaj.kundegorski@gmail.com>
2020-09-16 18:45:14 +0100

    simple variable timeline with bokeh and flask

---
+++ app/app.py
@@ -0,0 +1,51 @@
+# Boilerplate from https://github.com/dradecic
+
+import math
+import numpy as np
+import pandas as pd
+
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool, PrintfTickFormatter
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+
+from flask import Flask, render_template, request
+
+
+indicators_table = pd.read_csv('../data/keyvariables.csv')
+
+def one_variable_timeline(var_name, keyvariables):
+    p = figure(plot_width=400, plot_height=250, x_axis_type="datetime")
+    keyvariables['contact_day'] = pd.to_datetime(keyvariables['contact_day'])
+    p.line(keyvariables['contact_day'], keyvariables[var_name], color='navy', alpha=0.5)
+    p.circle(keyvariables['contact_day'], keyvariables[var_name], size=5, color='red', legend=var_name)
+    return p
+
+def redraw(indicator_name):
+    key_chart = one_variable_timeline(indicator_name,indicators_table)
+    return (
+        key_chart
+    )
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def chart():
+    indicators_list = indicators_table.columns
+
+    selected_indicator = request.form.get('dropdown-select')
+
+    key_chart = redraw(selected_indicator)
+
+    script_key_chart, div_key_chart = components(key_chart)
+
+    return render_template(
+        'index.html',
+        indicators_list=indicators_list,
+        div_key_chart=div_key_chart,
+        script_key_chart=script_key_chart,
+        selected_indicator=selected_indicator
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
